8695606aded8171ce59dbd55e3a40446a3ef5fda
Tobias Neitzel <tobias.neitzel@code-white.com>
2025-03-29 18:09:55 +0100

    Use json module instead of json2

--- src/settings-api.v
+++ src/settings-api.v
@@ -1,7 +1,7 @@
 module main
 
 import veb
-import x.json2
+import json
 
 // get_settings returns the currently configured rpv-web settings.
 @['/api/settings'; get]
@@ -14,10 +14,8 @@ pub fn (app &App) get_settings(mut ctx Context) veb.Result
 @['/api/settings'; post]
 pub fn (mut app App) set_settings(mut ctx Context) veb.Result
 {
-	if json_body := json2.raw_decode(ctx.req.data)
+	if json_map := json.decode(map[string]string, ctx.req.data)
 	{
-		json_map := json_body.as_map()
-
 		if symbol_path := json_map['symbol_path']
 		{
 		    app.symbol_resolver.symbol_path = symbol_path.str()
--- src/symbols-api.v
+++ src/symbols-api.v
@@ -2,7 +2,7 @@ module main
 
 import veb
 import toml
-import x.json2
+import json
 import qtc_de.rpv
 
 // update_symbols allows callers to merge an rpv-web symbol file with
@@ -10,11 +10,9 @@ import qtc_de.rpv
 @['/api/symbols'; post]
 pub fn (mut app App) update_symbols(mut ctx Context) veb.Result
 {
-	if json_body := json2.raw_decode(ctx.req.data)
+	if json_map := json.decode(map[string]string, ctx.req.data)
 	{
-		json_map := json_body.as_map()
-
-		if toml_data := toml.parse_text(json_map['symbols'].str())
+		if toml_data := toml.parse_text(json_map['symbols'])
 		{
 			symbol_resolver := rpv.parse_resolver(toml_data, app.symbol_resolver.symbol_path, '')
 			app.symbol_resolver.merge(symbol_resolver) or
